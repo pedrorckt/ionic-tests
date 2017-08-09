afeb35c0f19cd6357ce98767e54b9a955ce20662
Pedro Guglielmo <pedro@brandconnect.com.br>
2017-08-09 17:27:52 -0300

    Pause

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -16,14 +16,18 @@ export class HomePage {
   }
 
   play() {
-    let url = "http://classicoims.out.airtime.pro:8000/classicoims_a"; //"http://rckt.com.br/audio.mp3";
+    let url = "http://rckt.com.br/audio.mp3";
     this.audio = this.media.create(url);
-    // to listen to plugin events:
+    // subscribe to events
     this.audio.onStatusUpdate.subscribe(status => console.log('Status ' + status)); // fires when file status changes, BUT NOT STATUS 1
-    this.audio.onSuccess.subscribe(() => console.log('Action is successful')); // fires on stop/release, but not on play
+    this.audio.onSuccess.subscribe(() => console.log('Action is successful')); // fires on stop, but not on play/pause nor create
     this.audio.onError.subscribe(error => console.log('Error!'));
     // play the file
     this.audio.play();
+  }
+
+  pause() {
+    this.audio.pause();
   }
 
   stop() {
